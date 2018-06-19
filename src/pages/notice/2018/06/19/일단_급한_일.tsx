e373b00dc552a31e22314f7d6262c611a42a0066
Jell PD <>
2018-06-19 22:40:10 +0900

    posted '일단 급한 일'.

---
+++ src/pages/notice/2018/06/19/일단_급한_일.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Link from 'gatsby-link'
+
+const 일단_급한_일 = () => (
+  <div>
+    <h1>일단 급한 일</h1>
+    <p>분류를 정하고, 목록 뽑기</p>
+    <Link to="/">돌아가기</Link>
+  </div>
+)
+
+export default 일단_급한_일
