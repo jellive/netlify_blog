19a0a13e59fa310a9c3169c261eabfe92cce87ff
Jell PD <>
2018-06-24 22:23:45 +0900

    add graphql.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,16 @@ module.exports = {
     author: `Jell`
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/pages`
+      }
+    },
     `gatsby-plugin-react-helmet`,
     // Add typescript stack into webpack
-    `gatsby-plugin-typescript`
+    `gatsby-plugin-typescript`,
+    `gatsby-transformer-remark`
   ],
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,35 @@ import Link from 'gatsby-link'
 // to generate all types from graphQL schema
 interface IndexPageProps {
   data: {
+    // allFile: {
+    //   totalCount: number
+    //   edges: {
+    //     node: {
+    //       id: string
+    //       relativePath: string,
+    //       prettySize: string,
+    //       extension: string,
+    //       birthTime: string
+    //     }
+    //   }[]
+    // }
     site: {
       siteMetadata: {
         title: string
       }
+    }
+
+    allMarkdownRemark: {
+      edges: {
+        node: {
+          html: string
+          frontmatter: {
+            title: string
+            date(formatString: "MMMM DD, YYYY"): string
+            path: string
+          }
+        }
+      }[]
     }
   }
 }
@@ -26,6 +51,19 @@ export default class extends React.Component<IndexPageProps, {}> {
           <strong>{this.props.data.site.siteMetadata.title}</strong> site.
         </p>
         <p>Now go build something great.</p>
+        {/* {this.props.data.allFile.totalCount}개가 있습니다.<br /> */}
+        {
+          this.props.data.allMarkdownRemark.edges.map((edge) => (
+            <div key={edge.node.frontmatter.title} style={{borderStyle: 'dotted'}}>
+              <h3 style={{ marginBottom: 50 }}>
+                {edge.node.frontmatter.title}{" "}
+                <span style={{ color: '#bbb' }}>
+                  {edge.node.frontmatter.date}
+                </span>
+              </h3><br/>
+            </div>
+          ))
+        }
         <Link to="/notice/2018/06/19/일단_급한_일/">일단 급한 일</Link><br />
         <Link to="/notice/2018/06/17/시작/">시작</Link><br />
         <Link to="/page-2/">Go to page 2</Link><br />
@@ -36,10 +74,23 @@ export default class extends React.Component<IndexPageProps, {}> {
 
 export const pageQuery = graphql`
   query IndexQuery {
+    allMarkdownRemark {
+      edges {
+        node {
+          html
+          frontmatter {
+            # Assumes you're using title in your frontmatter.
+            title
+            date(formatString: "MMMM DD, YYYY")
+            path
+          }
+        }
+      }
+    }
     site {
       siteMetadata {
         title
       }
     }
-  }
+}
 `
